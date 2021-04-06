ea2bf60e12953f085e400d1a09b34ddf8bcc142b
unknown <chanakaminsky@gmail.com>
2021-04-05 22:15:25 -0400

    added canvas and basic drawing ability

--- src/main/java/kaminsky/paint/PaintCanvas.java
+++ src/main/java/kaminsky/paint/PaintCanvas.java
@@ -1,8 +1,30 @@
 package kaminsky.paint;
 
+import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
 
 public class PaintCanvas extends Canvas
 {
+    public void draw()
+    {
+        GraphicsContext context = getGraphicsContext2D();
+        context.setFill(Color.WHITE);
+        context.fillRect(0, 0, getWidth(), getHeight());
+        context.setStroke(Color.DARKRED);
+        context.setLineWidth(5);
+
+        addEventHandler(MouseEvent.MOUSE_DRAGGED,
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        context.strokeLine(event.getX(), event.getY(), event.getX(), event.getY());
+                    }
+                });
+    }
+
 
 }
--- src/main/java/kaminsky/paint/PaintController.java
+++ src/main/java/kaminsky/paint/PaintController.java
@@ -1,6 +1,15 @@
 package kaminsky.paint;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class PaintController
 {
+    @FXML
+    PaintCanvas paintCanvas;
 
+    public void initialize()
+    {
+        paintCanvas.draw();
+    }
 }
